8a963df9046a9021084a8d6e0770efb1e93ea97f
zackhitch <zackhitch@users.noreply.github.com>
2018-07-11 13:38:20 -0700

    changed from http to https

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { withRouter } from "react-router";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { withRouter } from 'react-router';
 
 import {
   Col,
@@ -9,33 +9,33 @@ import {
   FormGroup,
   Label,
   Input,
-  FormText
-} from "reactstrap";
+  FormText,
+} from 'reactstrap';
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password1: "",
-      password2: ""
+      username: '',
+      password1: '',
+      password2: '',
     };
   }
 
   signupHandler = event => {
     event.preventDefault();
     axios
-      .post("http://penpaldjango.herokuapp.com/rest-auth/registration/", {
+      .post('https://penpaldjango.herokuapp.com/rest-auth/registration/', {
         username: this.state.username,
         password1: this.state.password1,
-        password2: this.state.password2
+        password2: this.state.password2,
       })
       .then(response => {
-        console.log("New account created! Login to continue.");
-        this.props.history.push("/login");
+        console.log('New account created! Login to continue.');
+        this.props.history.push('/login');
       })
       .catch(error => {
-        alert("There was an error creating a new account");
+        alert('There was an error creating a new account');
       });
   };
 
