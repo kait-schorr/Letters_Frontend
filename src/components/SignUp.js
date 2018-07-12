5d2877d193e00beafa964911217534536f78246f
zackhitch <zackhitch@users.noreply.github.com>
2018-07-11 23:36:51 -0700

    centered signup form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -46,49 +46,51 @@ class SignUp extends Component {
   render() {
     return (
       <div className="signUpForm">
-        <Label>Sign Up!</Label>
-        <Form onSubmit={this.signupHandler} onChange={this.handleChange}>
-          <FormGroup row>
-            <Label for="signUpUsername" sm={2}>
-              Username
-            </Label>
-            <Col sm={10}>
-              <Input
-                type="text"
-                name="username"
-                id="signUpUsername"
-                placeholder="Enter your desired Username here"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="signUpPassword" sm={2}>
-              Password
-            </Label>
-            <Col sm={10}>
-              <Input
-                type="password"
-                name="password1"
-                id="signUpPassword"
-                placeholder="Please enter a password here"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="signUpPasswordConfirm" sm={2}>
-              Re-Password
-            </Label>
-            <Col sm={10}>
-              <Input
-                type="password"
-                name="password2"
-                id="signUpPasswordConfirm"
-                placeholder="Please confirm your password here"
-              />
-            </Col>
-          </FormGroup>
-          <Button>Submit</Button>
-        </Form>
+        <Col sm="12" md={{ size: 8, offset: 2 }}>
+          <h5>Sign Up:</h5>
+          <Form onSubmit={this.signupHandler} onChange={this.handleChange}>
+            <FormGroup row>
+              <Label for="signUpUsername" sm={2}>
+                Username
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type="text"
+                  name="username"
+                  id="signUpUsername"
+                  placeholder="Enter your desired Username here"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="signUpPassword" sm={2}>
+                Password
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type="password"
+                  name="password1"
+                  id="signUpPassword"
+                  placeholder="Please enter a password here"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="signUpPasswordConfirm" sm={2}>
+                Confirm
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type="password"
+                  name="password2"
+                  id="signUpPasswordConfirm"
+                  placeholder="Please confirm your password here"
+                />
+              </Col>
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </Col>
       </div>
     );
   }
