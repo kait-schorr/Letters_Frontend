c5473345f0b2befdc52dd0991ce0ead4c0ec2131
zackhitch <zackhitch@users.noreply.github.com>
2018-07-11 23:39:06 -0700

    centered login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 
-import axios from "axios";
+import axios from 'axios';
 import {
   Col,
   Button,
@@ -9,35 +9,35 @@ import {
   FormGroup,
   Label,
   Input,
-  FormText
-} from "reactstrap";
+  FormText,
+} from 'reactstrap';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: ""
+      username: '',
+      password: '',
     };
   }
 
   loginHandler = event => {
     event.preventDefault();
     axios
-      .post("https://penpaldjango.herokuapp.com/rest-auth/login/", {
+      .post('https://penpaldjango.herokuapp.com/rest-auth/login/', {
         username: this.state.username,
-        password: this.state.password
+        password: this.state.password,
       })
       .then(response => {
         this.props.setUser({
-          username: this.state.username
+          username: this.state.username,
         });
-        localStorage.setItem("authToken", response.data.key);
+        localStorage.setItem('authToken', response.data.key);
         console.log(response); //TODO:Put a route here
-        this.props.history.push("/penpals");
+        this.props.history.push('/penpals');
       })
       .catch(error => {
-        console.log("error:", error);
+        console.log('error:', error);
       });
   };
 
@@ -48,36 +48,38 @@ class Login extends Component {
   render() {
     return (
       <div className="logInForm">
-        <Label>Log In!</Label>
-        <Form onSubmit={this.loginHandler} onChange={this.handleChange}>
-          <FormGroup row>
-            <Label for="logInUsername" sm={2}>
-              Username
-            </Label>
-            <Col sm={10}>
-              <Input
-                type="text"
-                name="username"
-                id="logInUsername"
-                placeholder="Enter your Username here"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="logInPassword" sm={2}>
-              Password
-            </Label>
-            <Col sm={10}>
-              <Input
-                type="password"
-                name="password"
-                id="logInPassword"
-                placeholder="Please enter your password here"
-              />
-            </Col>
-          </FormGroup>
-          <Button>Submit</Button>
-        </Form>
+        <Col sm="12" md={{ size: 8, offset: 2 }}>
+          <h5>Log In:</h5>
+          <Form onSubmit={this.loginHandler} onChange={this.handleChange}>
+            <FormGroup row>
+              <Label for="logInUsername" sm={2}>
+                Username
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type="text"
+                  name="username"
+                  id="logInUsername"
+                  placeholder="Enter your Username here"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="logInPassword" sm={2}>
+                Password
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type="password"
+                  name="password"
+                  id="logInPassword"
+                  placeholder="Please enter your password here"
+                />
+              </Col>
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </Col>
       </div>
     );
   }
